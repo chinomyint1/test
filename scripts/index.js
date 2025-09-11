586640315d7ba2fb3f8e633c19f21bf0f2299408
chinomyint1 <1000041084@mcdmstudent.dk>
2025-09-11 13:15:26 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 import { footer } from "./component/footer.js";
 import { gallery } from "./component/gallery.js";
 import { slider } from "./component/slideshow.js";
-import { produktgruppe } from "./component/productgruppe.js";
 
 
 
 
 
 
-produktgruppe();
+
 footer();
 slider();
 gallery();

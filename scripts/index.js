04eed67d4cd6591553dd7a27ee5cfed4e4bcf14e
chinomyint1 <1000041084@mcdmstudent.dk>
2025-09-11 11:34:11 +0200

    404

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
 import { footer } from "./component/footer.js";
-import { produktgruppe } from "./component/productgruppe.js";
 import { gallery } from "./component/gallery.js";
 import { slider } from "./component/slideshow.js";
 
 footer();
-productgruppe();
 slider();
 gallery();

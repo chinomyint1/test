dbcafd9500af116bc13238cd75ec221137578fd0
chinomyint1 <1000041084@mcdmstudent.dk>
2025-09-11 10:00:38 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 import { footer } from "./component/footer.js";
-
+import { produktgruppe } from "./component/productgruppe.js";
 import { gallery } from "./component/gallery.js";
 import { slider } from "./component/slideshow.js";
 
 footer();
-
+productgruppe();
 slider();
 gallery();

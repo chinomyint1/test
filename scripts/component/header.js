84957d35f9baa1d66ba69d03d3b41452ba202aae
chinomyint1 <1000041084@mcdmstudent.dk>
2025-09-15 07:20:23 +0200

    h

---
+++ scripts/component/header.js
@@ -0,0 +1,29 @@
+const headerContainer = document.querySelector('.header-container')
+ 
+export const headerTmpl = () => {
+  return `
+    <div class="topbar">
+      <img src="assets/icons/truck-solid.svg" alt="">
+      <p>Bestiller du nu, vil din ordre blive sendt i dag</p>
+    </div>
+ 
+    <nav class="header-nav">
+      <a href="#">Vores Produktgrupper</a>
+      <a href="#">Vi Anbefaler</a>
+      <a href="#">Kontakt Os</a>
+      <div class="burger-menu" style="display: none;">  
+        <ul>
+          <li>Vores Produktgrupper</li>
+          <li>Vi Anbefaler</li>
+          <li>Kontakt-Os</li>
+        </ul>
+      </div>
+    </nav>
+  `
+}
+ 
+export const header = () => {
+  if (headerContainer) {
+    headerContainer.insertAdjacentHTML("beforeend", headerTmpl())
+  }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,9 @@
 import { footer } from "./component/footer.js";
 import { gallery } from "./component/gallery.js";
 import { slider } from "./component/slideshow.js";
-
-
-
-
-
-
+import { header } from "./component/header.js"
+ 
+header()
 
 footer();
 slider();
